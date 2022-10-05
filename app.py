cad0ec107afd8dba00ad5d35acb172aaf9f369e5
Jk950305 <jonk0305@gmail.com>
2022-10-04 21:38:15 -0700

    add evenly distributed costs for users and add summary page for individual contributions

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, request, flash, session
+from flask import Flask, render_template, request, flash, session, redirect, url_for
+import random
+import math
 
 app = Flask(__name__)
 app.secret_key = "tjdwlsldlqslek_9595959595"
 
 @app.route('/')
 def search():
+
     return render_template('index.html')
 
 @app.route('/trans', methods = ['GET', 'POST'])
 def trans():
     if request.method == 'GET':
-        return redirect(url_for('/'))
+        return redirect(url_for('search'))
     else:
         users = request.form.getlist('input_text[]')
         session['users'] = users
@@ -21,7 +24,7 @@ def trans():
 @app.route('/result', methods = ['GET', 'POST'])
 def results():
     if request.method == 'GET':
-        return redirect(url_for('/'))
+        return redirect(url_for('search'))
     else:
     	users = session.get('users', None)
     	lenders = request.form.getlist('lenders')
@@ -31,12 +34,12 @@ def results():
     	for i in range(len(lenders)):
     		string = 'checkboxes'+str(i+1)+'[]'
     		borrowers.append(request.form.getlist(string))
-
     		total = 0.0
-    	for j in range(len(amounts)):
-    		total = total+float(amounts[j])
+    		for j in range(len(amounts)):
+    			total = total+float(amounts[j])
 
     	trans = []
+    	summary = []
 
     	num = len(lenders)
     	if ( num!=len(borrowers) or num!=len(borrowers) or num!=len(borrowers) ):
@@ -49,13 +52,18 @@ def results():
 	    		if(borrowers[i]):tmp.append(borrowers[i])
 	    		trans.append(tmp)
 
-	    	texts = minCashFlow(createGraph(users, trans),users)
+	    	graph = createGraph(users, trans)
+	    	texts = minCashFlow(graph,users)
 	    	arr = texts.split(',')
 	    	result = arr[:-1]
 	    	if len(result)==0:
 	    		result= ['You guys are quits!']
+	    	else:
+	    		result.sort();
+
+	    	summary = totalExpenses(users,graph)
 	    	
-    	return render_template('result.html',result=result, total=("%.2f" % total))
+    	return render_template('result.html',result=result, total=("%.2f" % total), summary=summary)
 
 
 
@@ -119,24 +127,50 @@ def minCashFlow(graph,users):
     return minCashFlowRec(amount,users)
 
 
-
 def createGraph(users, trans):
-    #graph = [[0]*len(users)]*len(users)
     graph = [[0]*len(users) for _ in range(len(users))]
+
+    pq = sorted(users, key = lambda x: random.random())
 
     for aTran in trans:
         giver = aTran[0]
         total = aTran[1]
         borrowers = aTran[2]
-        split = total/(len(borrowers))
+        len_borrowers = len(borrowers)
+        split = total/len_borrowers
+
+        a = 100 * total
+        lo_val = math.floor(a/len_borrowers)
+        hi_val =  lo_val+1
+        num_hi = round(a%len_borrowers)
+        num_low = len_borrowers-num_hi
+
         col = users.index(giver)
-        for aBorrower in borrowers:
-            row = users.index(aBorrower)
-            graph[row][col] = graph[row][col]+split
+
+        cur_pq = pq.copy()
+        for aUser in cur_pq:
+            if aUser in borrowers:
+                row = users.index(aUser)
+                if num_hi>0 :
+                    graph[row][col] = graph[row][col]+hi_val/100
+                    num_hi = num_hi-1
+                    pq.append(pq.pop(0))
+                else :
+                    graph[row][col] = graph[row][col]+lo_val/100
+                
     return graph 
 
 
+def totalExpenses(users, graph):
+    lst = [[0]*2 for _ in range(len(users))]
+    count = 0
+    for row in graph:
+        for col in row:
+            lst[count][0] = users[count]
+            lst[count][1] = round(sum(graph[count]),2)
+        count = count + 1
 
+    return lst
      
 
 
